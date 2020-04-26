bb45120e39512d9e45c79f61b7415d9a151412e7
Carolina Aldana <19aldana@gmail.com>
2020-04-25 22:23:54 -0700

    update / last commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# code-quiz
+
+# Quiz Application
+A timed quiz application on JavaScript fundamentals that stores high scores
+
+## Introduction
+This application uses javascript to manipulate the DOM while a user takes a quiz. Localstorage is used to store highscores.
+
+### Functionality 
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+
+### Built With
+* Bootstrap
+* HTML
+* CSS
+* JavaScript
+* Git
---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+//Questions sourced from w3schools
+
+var questions = [
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: ["<scripting>", "<script>", "<javascript>","<js>"],
+    answer: "<script>"
+  },
+  {
+    title: "How to write an IF statement in JavaScript?",
+    choices: ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
+    answer: "if (i == 5)"
+  },
+  {
+    title: "How do you create a function in JavaScript?",
+    choices: ["function:myFunction()", "function = myFunction()", "function myFunction()","function:myFunction"],
+    answer: "function myFunction()"
+  },
+  {
+    title: "How does a WHILE loop start?",
+    choices: ["while i = 1 to 10", "while (i <= 10)", "(i <= 10)", "while (i <= 10; i++)"],
+    answer: "while (i <= 10)"
+  },
+  {
+    title: "How does a FOR loop start?",
+    choices: ["for (i = 0; i <= 5)", "i = 1 to 5", "for (i = 0; i <= 5; i++) ", "for i = 1 to 5"],
+    answer: "for (i = 0; i <= 5; i++)"
+  }
+]  
